d84c784e58b50e7604dffaf173111a2fb62763cb
daveotengo <daveotengo@gmail.com>
2021-02-18 01:18:13 +0000

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by David Oteng on 18/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
